7251cbced707c36f00616b38445e72add6b2eb15
CosmicCoder06 <k.avhi4612@gmail.com>
2025-12-02 15:42:01 +0530

    My-app

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -10,21 +10,28 @@ function App() {
       </h3>
       <div className="cards-container">
         <Card
-          title="Product 1"
+          // title="Product 1"
+          image="https://images.samsung.com/is/image/samsung/p6pim/in/feature/164976585/in-feature-galaxy-s-539720470?$FB_TYPE_A_MO_JPG$"
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          // link="https://www.samsung.com/in/smartphones/galaxy-s25/"
         />
         <Card
-          title="Product 2"
+          // title="Product 2"
+          image="https://images.samsung.com/in/smartphones/galaxy-s24/images/galaxy-s24-highlights-color-carousel-global-mo.jpg?imbypass=true"
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          // link="https://www.samsung.com/in/smartphones/galaxy-s24/"
         />
         <Card
-          title="Product 3"
+          // title="Product 3"
+          image="https://img.global.news.samsung.com/in/wp-content/uploads/2024/01/Galaxy-S24-Series_dl1.jpg"
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          // link="https://www.samsung.com/in/smartphones/galaxy-s24-ultra/"
         />
         <Card
-          title="Product 4"
+          // title="Product 4"
+          image="https://www.livemint.com/lm-img/img/2024/01/11/600x338/Samsung_Galaxy_S23_series_1675280781761_1704973663009.jpeg"
           description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-
+          // link="https://www.samsung.com/in/smartphones/galaxy-s23/"
         />
       </div>
     </div>
--- my-app/src/Components/Card.jsx
+++ my-app/src/Components/Card.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+// import "./Card.css";
 function Card(props) {
+  const link = props.link || "#";
+  const [first, second, ] = [1, 2, 3, 4, 5];
+  const [isClicked, setIsClicked] = React.useState(false);
+  
   return (
-    <div className = "card">
-      <img src="" alt="" />
+    <div className={`card${isClicked === true ? "blue-card": " "}`}>
       <h2>{props.title}</h2>
+      <img src={props.image} alt={props.title} />
       <p>{props.description}</p>
-      <button>Khreedna to padega na jii</button>
+      {/* <a href={link} target="_blank" rel="noopener noreferrer"> */}
+        <button type="button">Khreedna to padega na jii</button>
+      {/* </a> */}
     </div>
   );
 }
